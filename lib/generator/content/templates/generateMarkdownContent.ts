31bb1ad54ca86818fd2d59a73528fb4839702301
TheKeineAhnung <fireball73@gmx.de>
2022-04-17 01:07:49 +0200

    🎨 moved content creation for animations to external file

--- lib/generator/content/templates/generateMarkdownContent.ts
+++ lib/generator/content/templates/generateMarkdownContent.ts
@@ -1,3 +1,5 @@
+import { generateAnimationMarkdownContent } from './generateAnimationMarkdownContent';
+
 function generateMarkdownContent(
   type: string,
   selector: string,
@@ -13,25 +15,7 @@ function generateMarkdownContent(
     }
   });
   if (type.toLowerCase().trim().startsWith("animation")) {
-    let contentString: string = "";
-    tokens.forEach((token: string) => {
-      token.split("\n").forEach((line: string) => {
-        line.split("\r").forEach((line: string) => {
-          line = line.trim();
-          let start0To9: RegExp = new RegExp("^[0-9]");
-          if (
-            start0To9.test(line) ||
-            line.toLowerCase().startsWith("from") ||
-            line.toLowerCase().startsWith("to")
-          ) {
-            contentString += `- \`${line}\`:\n`;
-          } else {
-            contentString += `  - \`${line}\`\n`;
-          }
-        });
-      });
-    });
-    return `The \`${type.toLowerCase()}\` selector with the specified selectors \`${selector.trim()}\` has the following properties: \n\n${contentString}\n`;
+    return generateAnimationMarkdownContent(type, selector, tokens);
   } else {
     let contentString: string = "";
     tokens.forEach((token: string) => {
